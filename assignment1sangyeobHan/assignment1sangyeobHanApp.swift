913a04d1f0a39c9e69d1a522c14a1a5816ebd691
sy <s3821179@rmit.edu.vn>
2022-08-04 16:27:07 +0700

    Initial Commit

---
+++ assignment1sangyeobHan/assignment1sangyeobHanApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignment1sangyeobHanApp.swift
+//  assignment1sangyeobHan
+//
+//  Created by Sang Yeob Han  on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct assignment1sangyeobHanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
